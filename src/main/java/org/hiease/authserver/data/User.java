13f718e5cda290ed17a02b01b7801eab7900b82f
qihaiyan <haiyan_qi@hotmail.com>
2016-10-24 22:07:09 +0800

    chores

--- src/main/java/org/hiease/authserver/data/User.java
+++ src/main/java/org/hiease/authserver/data/User.java
@@ -1,98 +1,35 @@
 package org.hiease.authserver.data;
 
-import javax.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 @Entity
-public class User extends BaseEntity{
+public class User extends BaseEntity {
+
+    public static final PasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
 
     private String username;
-
     private String name;
-
     private String sex;
-
     private String status;
-
     private String email;
-
     private String mobile;
-
     private String department;
-
     private String isAdmin;
 
+    //    @JsonIgnore
     private String password;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public String getIsAdmin() {
-        return isAdmin;
-    }
-
-    public void setIsAdmin(String isAdmin) {
-        this.isAdmin = isAdmin;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
     public void setPassword(String password) {
-        this.password = password;
+        this.password = PASSWORD_ENCODER.encode(password);
     }
-
 }
--- src/main/java/org/hiease/authserver/data/UserRepository.java
+++ src/main/java/org/hiease/authserver/data/UserRepository.java
@@ -1,9 +1,20 @@
 package org.hiease.authserver.data;
 
-import org.springframework.data.repository.Repository;
+import java.util.Date;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface UserRepository extends Repository<User, String> {
+public interface UserRepository extends PagingAndSortingRepository<User, String> {
+	@Override
+	User save(@Param("user") User user);
 	User findByUsername(@Param("username") String username);
 	User findByUsernameAndStatus(@Param("username") String username, @Param("state") String state);
 }
