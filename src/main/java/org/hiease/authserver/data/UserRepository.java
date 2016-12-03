16e6d3729f5fa2ecbc3cf153dea6dec33e165cfe
qihaiyan <haiyan_qi@hotmail.com>
2016-12-03 22:46:30 +0800

    fix user query error

--- src/main/java/org/hiease/authserver/data/UserRepository.java
+++ src/main/java/org/hiease/authserver/data/UserRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -21,6 +23,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	User findByUsername(@Param("username") String username);
 
 	User findByUsernameAndStatus(@Param("username") String username, @Param("state") String state);
+
+	Page<User> findByUsernameContainsOrNameContainsOrMobileContains(@Param("username") String username, @Param("name") String name, @Param("mobile") String mobile, Pageable pageable);
 
 	@Modifying
 	@Query("update User u set u.password = :passwd where u.username = ?#{principal.username}")
